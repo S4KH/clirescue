ad6b20c20b7b7bdab7bc5e8791dee6523e97e5a5
Syecku <khusayan_syerikjan@unimedia.co.jp>
2016-11-02 13:07:22 +0800

    forking repo requires internal code changes

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"os"
 
-	"github.com/GoBootcamp/clirescue/trackerapi"
+	"github.com/s4kh/clirescue/trackerapi"
 	"github.com/codegangsta/cli"
 )
 
--- trackerapi/me.go
+++ trackerapi/me.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	u "os/user"
 
-	"github.com/GoBootcamp/clirescue/cmdutil"
-	"github.com/GoBootcamp/clirescue/user"
+	"github.com/s4kh/clirescue/cmdutil"
+	"github.com/s4kh/clirescue/user"
 )
 
 var (
